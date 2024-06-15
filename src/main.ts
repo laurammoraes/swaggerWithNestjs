1c40ca8dbd29898f12feb96b2920ef7094d4fde6
laurammoraes2@gmail.com <laura@poontoagencia.com>
2024-06-15 20:32:52 -0300

    implementar o swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { CompaniesModule } from './companies/companies.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(CompaniesModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Companies')
+    .setDescription('The companies API description')
+    .setVersion('1.0')
+    .addTag('companies')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
